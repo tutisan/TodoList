40951116310677773e109dbc43c0329d4c8cc93d
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-06 13:18:02 -0300

    created TodoListDbContext

---
+++ Data/TodoListDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data;
+
+public class TodoListDbContext : DbContext
+{
+    public DbSet<TaskItem> TaskItems { get; set;}
+
+    public string DbPath { get; }
+    
+    public TodoListDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "todolist.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data source={DbPath}");
+    }
+}
