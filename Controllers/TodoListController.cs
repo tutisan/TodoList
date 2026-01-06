e5e0ca6616c818bae2db640eca2090c32a5d56f4
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-06 13:34:38 -0300

    created unimplemented methods for TodoListController and changed its route

--- Controllers/TodoListController.cs
+++ Controllers/TodoListController.cs
@@ -1,11 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
 
 namespace TodoList.Controllers;
 
 [ApiController]
-[Route("/")]
+[Route("[controller]")]
 public class TodoListController : ControllerBase
 {
+    [HttpPost]
+    public IActionResult CreateTaskItem(TaskItem taskItem)
+    {
+        throw new NotImplementedException();
+    }
+
     [HttpGet]
-    public string Home() => "Hello, world!";
+    public IActionResult GetAllTaskItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{taskId}")]
+    public IActionResult GetTaskItemById(Guid taskId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{taskId}")]
+    public IActionResult UpdateTaskItem(Guid taskId, TaskItem taskItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{taskId}")]
+    public IActionResult DeleteTaskItem(Guid taskId)
+    {
+        throw new NotImplementedException();
+    }
 }
