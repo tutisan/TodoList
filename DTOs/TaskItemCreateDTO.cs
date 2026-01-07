545f206ca507dedd1427079be23de5535f955ac9
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-07 11:11:36 -0300

    created constructors to TaskItemCreateDTO

--- DTOs/TaskItemCreateDTO.cs
+++ DTOs/TaskItemCreateDTO.cs
@@ -5,4 +5,15 @@ public class TaskItemCreateDTO
 {
     public string Name { get; set; }
     public bool IsDone { get; set; }
+
+    public TaskItemCreateDTO(string name, bool isDone)
+    {
+        Name = name;
+        IsDone = isDone;
+    }
+
+    public TaskItemCreateDTO(string name)
+    {
+        Name = name;
+    }
 }
