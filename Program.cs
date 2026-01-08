efee8e22f4df00e026fa2c676b6491f1b80f0390
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-08 09:43:17 -0300

    added JWT authentication to project's services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 using TodoList.Data;
 
@@ -8,6 +10,17 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+               };
+            });
         builder.Services.AddControllers();
         builder.Services.AddDbContext<TodoListDbContext>();
 
