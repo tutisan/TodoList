c4532d471495889d55abd3c2a26357827287e76e
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-08 13:11:37 -0300

    renamed TaskItemDetailDTO to TaskDetailDTO

--- DTOs/TaskDetailDTO.cs
+++ DTOs/TaskDetailDTO.cs
@@ -1,7 +1,7 @@
 
 namespace TodoList.DTOs;
 
-public class TaskItemDetailDTO(string name, bool isDone)
+public class TaskDetailDTO(string name, bool isDone)
 {
     public string Name { get; set; } = name;
     public bool IsDone { get; set; } = isDone;
