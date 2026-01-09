1f92f0d58b198525d901decb43c574df108e76e9
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-09 11:08:00 -0300

    created migrations to foreign-key relationship addition

---
+++ Migrations/20260109140717_AddedRelationship.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AuthorId",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItems_AuthorId",
+                table: "TaskItems",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_Accounts_AuthorId",
+                table: "TaskItems",
+                column: "AuthorId",
+                principalTable: "Accounts",
+                principalColumn: "AccountId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_Accounts_AuthorId",
+                table: "TaskItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskItems_AuthorId",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "TaskItems");
+        }
+    }
+}
