7f0245fa9dd23a86f0c85409b8c4f5463c4877df
Tutisan <252955505+tutisan@users.noreply.github.com>
2026-01-09 11:04:14 -0300

    updates models to add foreign key relationship

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -6,4 +6,7 @@ public class TaskItem(string name, bool isDone)
     public Guid Id { get; set; }
     public string Name { get; set; } = name;
     public bool IsDone { get; set; } = isDone;
+
+    public Guid AuthorId { get; set; }
+    public Account Author { get; set; }
 }
